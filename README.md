bf96d28884ca2c646b9f24c60a2a7a3ff150cb96
keithowino <designsolutions1629@gmail.com>
2024-04-13 11:28:20 +0300

    Add modification and analyse error

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # THE COLLECTOR
+
+## Phase 1 Project
+
+### Overview
+
+This streaming site will utilize TMDB’s API to display movie and TV show information.
+
+### Features
+
+* Search Movies and TV Shows: 
+  * Users can search for their favorite movies and TV shows.
+* Display Details: 
+  * Show detailed information about each movie or TV show, including ratings, cast, and synopsis.
+* Watch Trailers: 
+  * Provide trailers for selected content.
+* Responsive Design: 
+  * Ensure the site works well on different devices.
+
+### Prerequisites
+
+...Specify any prerequisites or dependencies needed to run your project. For instance:
+
+1. Make sure you have Node.js is installed.
+2. You will have to obtain an API key from TMDB by signing up on
+their website.
+
+### Installation
+
+...Provide step-by-step instructions for setting up the project locally:
+...Clone this repository: git clone https://github.com/your-username/your-repo.git
+
+* Install dependencies on terminal using: npm install
+
+...Explain how to run the project:
+...Start the development server: npm start
+...Open the site in your browser: http://localhost:3000
+
+### Configuration
+
+...Describe any configuration options, such as environment variables or settings related to TMDB API usage.
+
+### API Integration
+
+...Provide details on how you’re using the TMDB API:
+
+Endpoints: List the relevant endpoints you’re using (e.g., /movie, /tv, /search).
+
+### Contributing
+
+...Encourage contributions from other developers. Explain how they can submit pull requests or report issues.
+
+### License
+
+...Specify the license under which your project is released (e.g., MIT, Apache, etc.).
+
+### Acknowledgments
+
+...Give credit to any third-party libraries, frameworks, or resources you’ve used.
--- script.js
+++ script.js
@@ -4,20 +4,23 @@ document.addEventListener("DOMContentLoaded", action);
 
 function action(){
   const appName = "the collector";
-  const imgBaseUrl = "http://image.tmdb.org/t/p/w500";
-  const config = "/configuration";
-
-  const trendingContainer = document.querySelector("#trending-list");
-  const upcomingContainer = document.querySelector("#upcoming-list");
-
-
   document.title = appName.toUpperCase();
-
   const siteTitleRef = document.querySelectorAll("#js-site-tile-ref");
 
   siteTitleRef.forEach((element) => {
     element.textContent = appName;
   })
+
+  const searchResultsContainer = document.querySelector(".searched-results");
+  const tvShowResults = document.querySelector(".tv-shows-results");
+  const movieResults = document.querySelector(".movie-results");
+  const personResults = document.querySelector(".person-results");
+  // const trendingContainer = document.querySelector("#trending-list");
+  // const upcomingContainer = document.querySelector("#upcoming-list");
+
+  const imgBaseUrl = "http://image.tmdb.org/t/p/w500";
+
+    const movieNameTvShowSearchBtnElement = document.querySelector("#movieNameTvShowSearchBtn");
 
   const options = {
     method: 'GET',
@@ -26,17 +29,118 @@ function action(){
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTdlMTFjYzUyNWE2ZGNmMGQxMjZmMDI5NDAzYjZjNCIsInN1YiI6IjY2MTdhMTc4NDUzOWQwMDE3ZGZjOTZhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.r7QXZxUOHWRdgh6irsq9gjafszv5kJHUp7crlIC2Flo'
     }
   };
+
+  // const searchInput = function () {
+  //   console.log(searchMovieNameTvShowInputStr);
+  //   fetch(`https://api.themoviedb.org/3/search/multi?query=${searchMovieNameTvShowInputStr}&include_adult=false&language=en-US&page=1`, options)
+  //     .then(response => response.json())
+  //     .then(response => console.log(response))
+  //     .catch(err => alert(err));
+  // };
+
+  movieNameTvShowSearchBtnElement.addEventListener("click", getMovieList);
+
+  function getMovieList(e){
+    e.preventDefault();
+    let searchMovieNameTvShowInputStr = document.querySelector("#movieNameTvShowInput").value.trim();
+    fetch(`https://api.themoviedb.org/3/search/multi?query=${searchMovieNameTvShowInputStr}&include_adult=false&language=en-US&page=1`, options)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+
+        const searchResultsArr = response.results;
+
+        searchResultsArr.forEach(element => {
+
+          let PosterImgUrl = imgBaseUrl + element.poster_path;
+          let ProfileImgUrl = imgBaseUrl + element.profile_path;
+
+          const tMediaTypeHTML = `
+          <div class="card new-item-card" style="width: 18rem;">
+            <img src="${PosterImgUrl}" class="card-img-top img-fluid" alt="${element.original_name} movie poster">
+            <div class="card-body">
+              <h5 class="card-title">${element.original_name}</h5>
+            </div>
+          </div>
+          `;
+          const PMediaTypeHTML = `
+          <div class="card new-item-card" style="width: 18rem;">
+            <img src="${ProfileImgUrl}" class="card-img-top img-fluid" alt="${element.name} profile picture">
+            <div class="card-body">
+              <h5 class="card-title">${element.name}</h5>
+            </div>
+          </div>
+          `;
+          const mMediaTypeHTML = `
+          <div class="card new-item-card" style="width: 18rem;">
+            <img src="${PosterImgUrl}" class="card-img-top img-fluid" alt="${element.title} movie poster">
+            <div class="card-body">
+              <h5 class="card-title">${element.title}</h5>
+            </div>
+          </div>
+          `;
+
+          switch (element.media_type) {
+            case "tv":
+              tvShowResults.innerHTML += tMediaTypeHTML;
+              break;
+          
+            case "person":
+              movieResults.innerHTML += PMediaTypeHTML;
+              break;
+
+            case "movie":
+              personResults.innerHTML += mMediaTypeHTML;
+              break;
+          }
+        })
+      })
+      .catch(err => alert(err));
+  };
+
+  /*
   
-  // TRENDING MOVIES
+  
+  movieNameTvShowSearchBtnElement.addEventListener("click", function () {
+    const searchMovieNameTvShowInputStr = document.querySelector("#movieNameTvShowInput").value.trim();
+    fetch(`https://api.themoviedb.org/3/search/multi?query=${searchMovieNameTvShowInputStr}&include_adult=false&language=en-US&page=1`, options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => alert(err));
+  });
+  
+  */
+
+  /*
+
+fetch('https://api.themoviedb.org/3/search/multi?include_adult=false&language=en-US&page=1', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+
+fetch('https://api.themoviedb.org/3/search/multi?query=Rober%20Downey&include_adult=false&language=en-US&page=1', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+  
+  
+  */
+  
+
+  /*
+
+  TRENDING MOVIES
   fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
     .then(response => response.json())
     .then(response => {
       const trendingMovieArr = response.results;
 
       trendingMovieArr.forEach(element => {
+        let imgUrl = imgBaseUrl + element.poster_path;
         let html = `
         <div class="card new-item-card" style="width: 18rem;">
-          <img src="${imgBaseUrl + element.poster_path}" class="card-img-top img-fluid" alt="${element.title} movie poster">
+          <img src="${imgUrl}" class="card-img-top img-fluid" alt="${element.title} movie poster">
           <div class="card-body">
             <h5 class="card-title">${element.title}</h5>
           </div>
@@ -48,10 +152,13 @@ function action(){
     })
     .catch(err => alert(err.message));
 
-    // UPCOMING MOVIES
+    */
 
-    let page = Math.floor(Math.random() * 61);
-    console.log("page " + page);
+    /*
+
+
+    UPCOMING MOVIES
+    let page = Math.floor(Math.random() * 62);
 
     fetch(`https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=${page}`, options)
     .then(response => response.json())
@@ -59,11 +166,10 @@ function action(){
       const upcomingMovieArr = response.results;
 
       upcomingMovieArr.forEach(element => {
-        let imgUrl;
         let html;
 
         if(element.poster_path !== null){
-          imgUrl = imgBaseUrl + element.poster_path;
+          let imgUrl = imgBaseUrl + element.poster_path;
           html = `
           <div class="card new-item-card" style="width: 18rem;">
             <img src="${imgUrl}" class="card-img-top img-fluid" alt="${element.title} movie poster">
@@ -73,12 +179,12 @@ function action(){
             </div>
           </div>
           `;
-        }else{
-          html = "";
-        }
+        };
 
         upcomingContainer.innerHTML += html;
       });
     })
     .catch(err => alert(err.message));
+
+    */
 };
